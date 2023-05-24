fc426011582c7d77a2c200a622714ae90e4f81c3
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-05-24 11:13:57 -0300

    continuando

--- praticas/exe01/scripts/script.js
+++ praticas/exe01/scripts/script.js
@@ -5,5 +5,10 @@ var fundo = window.document.body
 var imagem = window.document.getElementById('imagem')
 
 if (horario < 12) {
-    fundo.style.backgroundImage = 'linear'
+    fundo.style.backgroundImage = 'linear-gradient(to right, orange, yellow)'
+    imagem.
+} else if (horario <= 18) {
+    fundo.style.backgroundImage = 'linear-gradient(to bottom, darkorange, orange)'
+} else {
+    fundo.style.backgroundImage = 'linear-gradient(to right, gray, black)'
 }
