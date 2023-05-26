3814628b197da8ae953661bfea4494f50cd21e49
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-05-26 08:28:27 -0300

    feito o primeiro exercicio

--- praticas/exe01/scripts/script.js
+++ praticas/exe01/scripts/script.js
@@ -1,14 +1,21 @@
-var agora = new Date()
-var horario = agora.getHours()
+function carregar() {
+    var agora = new Date()
+    var horario = agora.getHours()
 
-var fundo = window.document.body
-var imagem = window.document.getElementById('imagem')
+    var fundo = window.document.body
+    var paragrafo = window.document.querySelector('div > p')
+    var imagem = window.document.getElementById('imagem')
 
-if (horario < 12) {
-    fundo.style.backgroundImage = 'linear-gradient(to right, orange, yellow)'
-    imagem.
-} else if (horario <= 18) {
-    fundo.style.backgroundImage = 'linear-gradient(to bottom, darkorange, orange)'
-} else {
-    fundo.style.backgroundImage = 'linear-gradient(to right, gray, black)'
+    paragrafo.innerHTML = `Agora são <strong>${horario}</strong> horas`
+
+    if (horario < 12) {
+        fundo.style.backgroundImage = 'linear-gradient(to right, orange, yellow)'
+        imagem.setAttribute('src', 'imagens/manha.png')
+    } else if (horario < 18) {
+        fundo.style.backgroundImage = 'linear-gradient(to bottom, darkorange, orange)'
+        imagem.setAttribute('src', 'imagens/tarde.png')
+    } else {
+        fundo.style.backgroundImage = 'linear-gradient(to right, dimgray, black)'
+        imagem.setAttribute('src', 'imagens/noite.png')
+    }
 }
