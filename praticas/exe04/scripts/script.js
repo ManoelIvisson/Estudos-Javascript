06f43c791d33863a48328a63b3328bac87baf64c
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-05-30 20:03:08 -0300

    Feito o exercício da tabuada

--- praticas/exe04/scripts/script.js
+++ praticas/exe04/scripts/script.js
@@ -1,36 +1,20 @@
-var botao = document.getElementById('botao')
-botao.addEventListener('click', contar)
+function calculaTabuada() {
+    let valorStr = document.getElementById('num')
+    let valor = Number(valorStr.value)
+    let tabuada = document.querySelector('div#div_tabuada > select')
 
-var valorIncio = document.getElementById('inicio')
-var valorFim = document.getElementById('fim')
-var valorPasso = document.getElementById('passo') 
+    if (valorStr.value != ''){
+        tabuada.innerHTML = ''
 
-var localConta = document.querySelector('div#contagem > p')
-
-function contar(){
-    localConta.innerHTML = 'Contando... </br>'
-    
-    if (valorIncio.value == '' || valorFim.value == '' || valorPasso.value == '') {
-        localConta.innerHTML = 'Impossível contar'
+        for (c = 1; c <= 10; c++) {
+            let multiplicacao = document.createElement('option')
+            multiplicacao.value = `tab${c}`
+            let calculo = document.createTextNode(`${valor} x ${c} = ${valor * c}`)
+            
+            multiplicacao.appendChild(calculo)
+            tabuada.appendChild(multiplicacao)
+        }
     } else {
-        let inicio = Number(valorIncio.value)
-        let fim = Number(valorFim.value)
-        let passo = Number(valorPasso.value)
-
-        if (passo <= 0) {
-            alert('Impossível contar de 0 em 0 ou com número negativo, o valor foi mudado para 1')
-            passo = 1
-        }
-
-        if (inicio < fim) { //Contagem crescente
-            for (let c = inicio; c <= fim; c += passo) {
-                localConta.innerHTML += ` ${c} &#x1F449; `
-            }
-        } else { //Contagem decrescente
-            for (let c = inicio; c >= fim; c -= passo) {
-                localConta.innerHTML += ` ${c} &#x1F449; `
-            }
-        }
-        localConta.innerHTML += 'Fim &#x1F3C1;'
+        alert('Por favor, digite um valor')
     }
 }
